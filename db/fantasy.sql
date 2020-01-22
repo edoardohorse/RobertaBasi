f5e5b9e2f6fc8d02b5fe95a8cd2c1ab8cbaff2ad
Edoardo Cavallo <iweek16@gmail.com>
2020-01-22 10:09:22 +0100

    Fix problemi

--- db/fantasy.sql
+++ db/fantasy.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 21, 2020 alle 17:54
--- Versione del server: 10.1.13-MariaDB
--- Versione PHP: 7.2.10
+-- Creato il: Gen 21, 2020 alle 18:27
+-- Versione del server: 10.1.36-MariaDB
+-- Versione PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `fantasy`
 --
+CREATE DATABASE IF NOT EXISTS `fantasy` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `fantasy`;
 
 -- --------------------------------------------------------
 
@@ -37,14 +39,6 @@ CREATE TABLE `abbonamentoattivo` (
   `idCliente` int(11) NOT NULL,
   `idtipologia` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `abbonamentoattivo`
---
-
-INSERT INTO `abbonamentoattivo` (`id`, `dataInizio`, `dataFine`, `ingressiRimanenti`, `idCliente`, `idtipologia`) VALUES
-(1, '2020-01-18', '2020-02-29', 12, 1, 1),
-(23, '2020-01-20', '2020-12-31', 24, 37, 1);
 
 --
 -- Trigger `abbonamentoattivo`
@@ -95,14 +89,6 @@ CREATE TABLE `attrazione` (
   `orari` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dump dei dati per la tabella `attrazione`
---
-
-INSERT INTO `attrazione` (`id`, `nome`, `tipologia`, `longitudine`, `latitudine`, `maxPosti`, `etaMin`, `altezzaMin`, `orari`) VALUES
-(1, 'Katoon', 'Giostra', 40.2548, 42.375, 45, 14, 150, NULL),
-(2, 'Sotto il mare', 'Spettacolo', 40.2548, 42.375, 250, NULL, NULL, '18:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -138,18 +124,6 @@ CREATE TABLE `cliente` (
   `sesso` enum('M','F','Und') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dump dei dati per la tabella `cliente`
---
-
-INSERT INTO `cliente` (`id`, `nome`, `cognome`, `dataNascita`, `sesso`) VALUES
-(1, 'Anna', 'Spagna', '0000-00-00', 'F'),
-(2, 'Claudio', 'Angelastro', '0000-00-00', 'M'),
-(4, 'Sara', 'Gigli', '0000-00-00', 'F'),
-(6, 'Giovanna', 'Basile', '0000-00-00', 'F'),
-(37, 'Edoardo', 'Cavallo', '0000-00-00', 'M'),
-(39, 'Rob', 'Della R', '0000-00-00', 'Und');
-
 -- --------------------------------------------------------
 
 --
@@ -168,16 +142,6 @@ CREATE TABLE `dipendente` (
   `dataNascita` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dump dei dati per la tabella `dipendente`
---
-
-INSERT INTO `dipendente` (`id`, `nome`, `cognome`, `stipendio`, `dataInizio`, `dataFine`, `sesso`, `dataNascita`) VALUES
-(1, 'Claudio', 'Scialpi', 900, '2017-10-16', '2019-03-15', 'M', '0000-00-00'),
-(2, 'Serena', 'Giusti', 790, '2019-04-01', '2020-04-03', 'F', '0000-00-00'),
-(3, 'Angelo', 'De Michele', 1200, '2020-01-13', '2021-01-11', 'M', '0000-00-00'),
-(4, 'Stella', 'De Giorgi', 1100, '2019-07-01', '2020-05-29', 'F', '0000-00-00');
-
 -- --------------------------------------------------------
 
 --
@@ -188,27 +152,6 @@ DROP TABLE IF EXISTS `gestione`;
 CREATE TABLE `gestione` (
   `idDipendente` int(11) NOT NULL,
   `idAttrazione` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `gestione`
---
-
-INSERT INTO `gestione` (`idDipendente`, `idAttrazione`) VALUES
-(1, 1),
-(1, 2),
-(3, 1);
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `inclusione`
---
-
-DROP TABLE IF EXISTS `inclusione`;
-CREATE TABLE `inclusione` (
-  `idAbbonamento` int(11) NOT NULL,
-  `idBiglietto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -228,13 +171,6 @@ CREATE TABLE `storicoabbonamento` (
   `idtipologia` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dump dei dati per la tabella `storicoabbonamento`
---
-
-INSERT INTO `storicoabbonamento` (`id`, `dataInizio`, `dataFine`, `costo`, `ingressiRimanenti`, `idCliente`, `idtipologia`) VALUES
-(20, '2020-01-21', '2020-05-21', 200, 0, 39, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -250,14 +186,6 @@ CREATE TABLE `tipologia` (
   `ingressiTotali` int(11) NOT NULL,
   `durata` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `tipologia`
---
-
-INSERT INTO `tipologia` (`id`, `nome`, `annoCreazione`, `costo`, `ingressiTotali`, `durata`) VALUES
-(1, 'Annuale', 2017, 480, 30, 0),
-(2, 'Quadrimestrale', 2019, 200, 15, 0);
 
 -- --------------------------------------------------------
 
@@ -330,13 +258,6 @@ ALTER TABLE `gestione`
   ADD KEY `idAttrazione` (`idAttrazione`);
 
 --
--- Indici per le tabelle `inclusione`
---
-ALTER TABLE `inclusione`
-  ADD PRIMARY KEY (`idAbbonamento`,`idBiglietto`),
-  ADD KEY `idBiglietto` (`idBiglietto`);
-
---
 -- Indici per le tabelle `storicoabbonamento`
 --
 ALTER TABLE `storicoabbonamento`
@@ -365,7 +286,100 @@ ALTER TABLE `vip`
 -- AUTO_INCREMENT per la tabella `abbonamentoattivo`
 --
 ALTER TABLE `abbonamentoattivo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `attrazione`
+--
+ALTER TABLE `attrazione`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `biglietto`
+--
+ALTER TABLE `biglietto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `dipendente`
+--
+ALTER TABLE `dipendente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `storicoabbonamento`
+--
+ALTER TABLE `storicoabbonamento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `tipologia`
+--
+ALTER TABLE `tipologia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `vip`
+--
+ALTER TABLE `vip`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `abbonamentoattivo`
+--
+ALTER TABLE `abbonamentoattivo`
+  ADD CONSTRAINT `abbonamentoattivo_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `abbonamentoattivo_ibfk_2` FOREIGN KEY (`idtipologia`) REFERENCES `tipologia` (`id`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `accesso`
+--
+ALTER TABLE `accesso`
+  ADD CONSTRAINT `accesso_ibfk_1` FOREIGN KEY (`idAttrazione`) REFERENCES `attrazione` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `accesso_ibfk_2` FOREIGN KEY (`idVip`) REFERENCES `vip` (`id`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `biglietto`
+--
+ALTER TABLE `biglietto`
+  ADD CONSTRAINT `biglietto_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `biglietto_ibfk_3` FOREIGN KEY (`idAbbonamento`) REFERENCES `abbonamentoattivo` (`id`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `gestione`
+--
+ALTER TABLE `gestione`
+  ADD CONSTRAINT `gestione_ibfk_1` FOREIGN KEY (`idDipendente`) REFERENCES `dipendente` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `gestione_ibfk_2` FOREIGN KEY (`idAttrazione`) REFERENCES `attrazione` (`id`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `storicoabbonamento`
+--
+ALTER TABLE `storicoabbonamento`
+  ADD CONSTRAINT `storicoabbonamento_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `storicoabbonamento_ibfk_2` FOREIGN KEY (`idtipologia`) REFERENCES `tipologia` (`id`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `vip`
+--
+ALTER TABLE `vip`
+  ADD CONSTRAINT `vip_ibfk_1` FOREIGN KEY (`idBiglietto`) REFERENCES `biglietto` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- NT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT per la tabella `attrazione`
@@ -413,54 +427,6 @@ ALTER TABLE `vip`
 -- Limiti per le tabelle scaricate
 --
 
---
--- Limiti per la tabella `abbonamentoattivo`
---
-ALTER TABLE `abbonamentoattivo`
-  ADD CONSTRAINT `abbonamentoattivo_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `abbonamentoattivo_ibfk_2` FOREIGN KEY (`idtipologia`) REFERENCES `tipologia` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `accesso`
---
-ALTER TABLE `accesso`
-  ADD CONSTRAINT `accesso_ibfk_1` FOREIGN KEY (`idAttrazione`) REFERENCES `attrazione` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `accesso_ibfk_2` FOREIGN KEY (`idVip`) REFERENCES `vip` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `biglietto`
---
-ALTER TABLE `biglietto`
-  ADD CONSTRAINT `biglietto_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `biglietto_ibfk_3` FOREIGN KEY (`idAbbonamento`) REFERENCES `abbonamentoattivo` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `gestione`
---
-ALTER TABLE `gestione`
-  ADD CONSTRAINT `gestione_ibfk_1` FOREIGN KEY (`idDipendente`) REFERENCES `dipendente` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `gestione_ibfk_2` FOREIGN KEY (`idAttrazione`) REFERENCES `attrazione` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `inclusione`
---
-ALTER TABLE `inclusione`
-  ADD CONSTRAINT `inclusione_ibfk_1` FOREIGN KEY (`idAbbonamento`) REFERENCES `abbonamentoattivo` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `inclusione_ibfk_2` FOREIGN KEY (`idBiglietto`) REFERENCES `biglietto` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `storicoabbonamento`
---
-ALTER TABLE `storicoabbonamento`
-  ADD CONSTRAINT `storicoabbonamento_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `storicoabbonamento_ibfk_2` FOREIGN KEY (`idtipologia`) REFERENCES `tipologia` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `vip`
---
-ALTER TABLE `vip`
-  ADD CONSTRAINT `vip_ibfk_1` FOREIGN KEY (`idBiglietto`) REFERENCES `biglietto` (`id`) ON DELETE CASCADE;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
